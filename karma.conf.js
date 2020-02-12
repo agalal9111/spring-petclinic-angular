89ecac6de78d58e1e6425afb8266b17a1b11e799
agalal9111 <32462505+agalal9111@users.noreply.github.com>
2020-02-12 12:45:46 +0200

    Update karma.conf.js

--- karma.conf.js
+++ karma.conf.js
@@ -25,14 +25,10 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    //browsers: ['Chrome'],
-    browsers: ['ChromeHeadless'],
+    browsers: ['Chrome'],
     customLaunchers: {
-      ChromeHeadlessNoSandbox: {
-        base: 'ChromeHeadless',
-        flags: ['--no-sandbox']
-      }
-    },
+      flags: ['--no-sandbox']
+    }
     singleRun: false
   });
 };
